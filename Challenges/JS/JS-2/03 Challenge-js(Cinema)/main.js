e7575570ec958facf7f92a57b01b7482eab357ae
bspoloo <animetx098@gmail.com>
2024-07-16 12:30:51 -0400

    Challenge about cinema is done

--- Challenges/JS/JS-2/03 Challenge-js(Cinema)/main.js
+++ Challenges/JS/JS-2/03 Challenge-js(Cinema)/main.js
@@ -61,8 +61,6 @@ class Cinema {
 const cine = new Cinema(5, 5);
 cine.reserve(2, 2);
 cine.reserve(1, 2);
-cine.reserve(3, 1);
-cine.reserve(1, 2);
 cine.reserve(1, 6);
 cine.reserve(0, 6);
 cine.showCine()
